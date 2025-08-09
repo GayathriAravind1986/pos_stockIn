1c86bb92669ab401d4f243a6c8a6d28d324a777a
DELL <kousi.gayu12@gmail.com>
2025-08-09 13:24:24 +0530

    Your commit message

--- lib/Bloc/theme_cubit.dart
+++ lib/Bloc/theme_cubit.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:simple/Reusable/color.dart';
 
-
 /// ThemeCubit Class
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(_lightTheme);
